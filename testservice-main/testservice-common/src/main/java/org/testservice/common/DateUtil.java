9089ce345f0f7d3c8c993be4f71979d4aac5e36d
test <canini9720@gmail.com>
2018-06-03 09:17:34 +0400

    testmain

---
+++ testservice-main/testservice-common/src/main/java/org/testservice/common/DateUtil.java
@@ -0,0 +1,38 @@
+package org.testservice.common;
+
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateUtil {
+	
+	public static final String FORMAT_DDMMYYYY_HHMMSS = "dd/MM/yyyy HH:mm:ss";
+
+	public static Date getDate(String dateValue,String format) throws ParseException{
+        SimpleDateFormat sdf = new SimpleDateFormat(format,Locale.ENGLISH);
+        return  sdf.parse(dateValue);
+	}
+	
+	public static Date getDateURIDecoded(String dateValue,String format) throws ParseException, UnsupportedEncodingException{
+        SimpleDateFormat sdf = new SimpleDateFormat(format,Locale.ENGLISH);
+        return  sdf.parse(java.net.URLDecoder.decode(dateValue, "UTF-8"));
+	}	
+	
+	
+	public static String dateConvert(String pattern){
+
+        long sysdate = System.currentTimeMillis();
+        Date sys_date = new Date(sysdate);
+        String system_date ="";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        
+       return system_date = formatter.format(sys_date);	
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
